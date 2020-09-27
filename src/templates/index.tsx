e7532dba0fab9a37e3f1a1b390f6e0698d845e00
Victor Dozal <v.dozal@live.com>
2020-09-26 23:50:18 -0500

    📦 NEW: Replaced logo with org's title

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -101,7 +101,7 @@ const IndexPage: React.FC<IndexProps> = props => {
             <SiteNav isHome />
             <SiteHeaderContent className="site-header-conent">
               <SiteTitle className="site-title">
-                {props.data.logo ? (
+                {/* {props.data.logo ? (
                   <img
                     style={{ maxHeight: '55px' }}
                     src={props.data.logo.childImageSharp.fixed.src}
@@ -109,7 +109,8 @@ const IndexPage: React.FC<IndexProps> = props => {
                   />
                 ) : (
                   config.title
-                )}
+                )} */}
+                {config.title}
               </SiteTitle>
               <SiteDescription>{config.description}</SiteDescription>
             </SiteHeaderContent>
